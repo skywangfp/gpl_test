6a88a7c34a61e0d399fdee7145253686496b9514
sky.wang <sky.wang@flexport.com>
2019-06-25 20:08:22 +0800

    1. 嵌套参数 {   book(id:1) {     id     name     comments(pageSize:10) {       content     } }

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -5,7 +5,15 @@ class Types::BookType < Types::BaseObject
   field :name, String, null: false
   field :author, String, null: false
   field :price, Float, null: false
-  field :comments, [Types::CommentType], null: true
+  field :comments, [Types::CommentType], null: true do
+    description 'comments'
+    argument :pageSize, Integer, required: false
+    argument :pageNo, Integer, required: false
+  end
+
+  def comments(page_size: nil, page_no: nil)
+    object.comments.filter_page(page_size, page_no)
+  end
     # And fields can have their own descriptions:
     # description: "This post's comments, or null if this post has comments disabled."
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :book
+
+  scope :filter_len, ->(len) { limit(len) if len.to_i > 0}
 end
